d41775754edd9a366255ff0a76ed30d7ce867c15
mashroofmashru <mashroofvlk@gmail.com>
2023-09-13 23:29:27 +0530

    user valid or invalid checking and display error message

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var productHelpers=require('../helpers/product-helpers');
 var userHelpers=require('../helpers/user-helpers');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let user=req.session.user;
@@ -15,7 +16,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login',(req,res)=>{
-  res.render('user/login')
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else
+  res.render('user/login',{"loginErr":req.session.loginErr})
+  req.session.loginErr=false;
 });
 
 
@@ -24,29 +29,35 @@ router.get('/signup',(req,res)=>{
 });
 
 router.post('/signup',(req,res)=>{
+
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response)
   });
+  
 });
 
 router.post('/login',(req,res)=>{
+
   userHelpers.doLogin(req.body).then((response)=>{
     if(response.status){
-      req.session.logged=true;
+      req.session.loggedIn=true;
       req.session.user=response.user;
       res.redirect('/')
     }else{
+      req.session.loginErr="Invalid username or password";
       res.redirect('/login')
     }
-
   });
 
 });
 
 router.get('/logout',(req,res)=>{
+  
   req.session.destroy();
   res.redirect('/')
 
 });
+
+
 
 module.exports = router;
